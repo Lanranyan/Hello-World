0fb4961a6cc9a28a97cdc620f51fe0800d670914
Lanranyan <44810787+Lanranyan@users.noreply.github.com>
2019-01-11 09:59:29 -0700

    Add files via upload

---
+++ Assignment X.py
@@ -0,0 +1,26 @@
+'''
+a) 1
+   22
+   333
+   4444
+b)     1
+      22
+     333
+    4444
+   55555
+c) Create a function to evaluate the distance between two coordinate points using distance formula
+   distance(x1, x2, y1, y2 )
+'''
+
+x = 0
+rep = 1
+n = int(input("How many times?"))
+
+while rep <= n:
+    print(x+1)
+    x = x + 1
+    rep = rep + 1
+    
+
+
+
---
+++ Calc.py
@@ -0,0 +1,7 @@
+# print("2+3")
+# print(2+3)
+print("Your new score is", "1030 + 10")
+print("Your new score is", 1030 + 10)
+print("Your new score is,", 1030 + 10)
+print(" I want to print a double quote \" for some reason.") #Must have quotes around it to show up double-quote
+print("\"The little sheep rolled down the hill. \"")
