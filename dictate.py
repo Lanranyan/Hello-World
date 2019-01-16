bb1feef0d7a73c8415c3822175700a458df03933
Lanranyan <44810787+Lanranyan@users.noreply.github.com>
2019-01-16 09:33:40 -0700

    Add files via upload

---
+++ dictate.py
@@ -0,0 +1,15 @@
+''' Dictionaries '''
+
+dr = {'joke': 'farce',
+     'talk': 'whisper',
+     'chick': 'hen',
+      'light up': 'ignite'}
+print(dr)
+
+print(dr['joke'])
+print(dr['light up'])
+
+### show the available keys in the dictionary
+print(dr.keys())
+
+###
