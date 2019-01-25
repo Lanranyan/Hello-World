04b62588d7aa831da0912ad4ee1865339472b3cd
Lanranyan <44810787+Lanranyan@users.noreply.github.com>
2019-01-25 08:44:44 -0700

    Add files via upload

--- Script.py
+++ Script.py
@@ -1,8 +1,10 @@
-
-part1 = " In the dark, cold night, comes the phantoms of the night. They lurk around the corners of alleys and their " \
+text = " In the dark, cold night, comes the phantoms of the night. They lurk around the corners of alleys and their " \
         "agility is quicker than the mind. Once followed, begins an endless chase  "
-print(part1)
+print(text)
+changes = {'dark': 'light','comes':'arrives','mind':'wits'}
 
-changes = {'dark': input("An adjective"),'comes':'arrives','mind':'wits'}
-
-print(part1)
+list_text = text.split()
+new_list = []
+for word in list_text:
+    translation = changes.get(word)
+    new_list.append(translation if  )
